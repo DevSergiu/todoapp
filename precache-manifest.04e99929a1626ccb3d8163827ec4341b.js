95cea8c40715d1e144bc86a17857f9bfbbd7f038
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-06-17 08:56:50 +0300

    Updates

---
+++ precache-manifest.04e99929a1626ccb3d8163827ec4341b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51965e1ad3a87e4f85159b22da2b1c2a",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/todoapp/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "7b766972ec6e9c1286af",
+    "url": "/todoapp/static/js/main.81e192b3.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
