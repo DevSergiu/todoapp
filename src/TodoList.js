fedb3ca57a84c08046e7fe713ef75677a374ab8c
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-08-11 22:13:10 +0300

    add form and delete todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
+import NewTodoForm from "./NewTodoForm"
 import Todo from "./Todo";
 
 export default class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = { todos: [{task: "Walk "}, {task: " Onother task"}] }
+    this.state = {
+      todos : []
+    };
+    this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+  remove(id) {
+    this.setState ({
+      todos : this.state.todos.filter( t => t.id !== id)
+    })
   }
   render() {
     const todos = this.state.todos.map(todo => {
-      return <Todo task={todo.task} />;
+      return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} />;
     })
     return (
       <div>
         <h1>Todo List!</h1>
+        <NewTodoForm createTodo={this.create} />
         <ul>
           {todos}
         </ul>
